3109d15896b2e96116cac96736360fa4c72c567f
Ben Sirivallop <bensirivallop@Bens-MacBook-Air.local>
2023-06-06 17:41:30 -0700

    yay home page works

--- tutorial1.py
+++ tutorial1.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
-
-#create route    
-@app.route("/") #prob should be /home but whatever
 #differnt page
+#create route    
+#prob should be /home but whatever
+@app.route("/") 
 def home():
     return "Hello, it's home <h3>hi<h3>"
+
+if __name__ == "__main__":
+    app.run()
