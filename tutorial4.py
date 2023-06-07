6b406e316dea79cdee7b1c079e2a18ce3b95797b
Ben Sirivallop <bensirivallop@Bens-MacBook-Air.local>
2023-06-07 08:56:25 -0700

    basic form

--- tutorial4.py
+++ tutorial4.py
@@ -1,10 +1,19 @@
 from flask import Flask, url_for, redirect, render_template
-
+#posting and getting  
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/<usr>/")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
