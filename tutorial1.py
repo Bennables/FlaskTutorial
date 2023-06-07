5e5814cb909714c7af854bdd350f611b77a0bf1e
Ben Sirivallop <bensirivallop@Bens-MacBook-Air.local>
2023-06-06 17:59:42 -0700

    found a way to route to <>

--- tutorial1.py
+++ tutorial1.py
@@ -1,5 +1,8 @@
 from flask import Flask, redirect, url_for
 
+# if app.route has / before and after, url can be with / or not
+
+
 app = Flask(__name__)
 
 #differnt page
@@ -12,14 +15,20 @@ def home():
 
 #another page. This one will give us <name> as the param
 #prints Hello (value of name) when at 127.0.0.1:5000/(value of name)
-@app.route("/<name>")
+@app.route("/<name>/")
 def user(name):
     return f"Hello {name}"
 
 #demoing a redirect (cases where bad credentials)
-@app.route("/admin")
+@app.route("/admin/")
 def admin():
+    #can't use user yet
     return redirect(url_for("home"))
+
+@app.route("/admin1/")
+def admin1():
+    print("redirected")
+    return redirect(url_for("user", name = 'Admin1'))
 
 
 if __name__ == "__main__":
