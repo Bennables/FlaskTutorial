b5a85518dceb4aad8ab5e2f0a6e9990750ec5cf4
Ben Sirivallop <bensirivallop@Bens-MacBook-Air.local>
2023-06-07 08:03:48 -0700

    can use different html files with template

--- tutorial3.py
+++ tutorial3.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("newindex.html")
+    return render_template("indexTut3.html")
+
+@app.route("/two/")
+def two():
+    return render_template("indexTut3-2.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
