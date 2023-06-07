262fa2ba34108b7a5f27d6af7834ae74119ad8a5
Ben Sirivallop <bensirivallop@Bens-MacBook-Air.local>
2023-06-06 17:28:37 -0700

    creating home page

--- tutorial1.py
+++ tutorial1.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run()
+
+#create route    
+@app.route("/") #prob should be /home but whatever
+#differnt page
+def home():
+    return "Hello, it's home <h3>hi<h3>"
